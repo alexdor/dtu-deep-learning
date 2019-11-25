f01961a126772ba633e47821188b2264daddc860
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-25 15:11:05 +0100

    feat: Batch all the things

--- data_generator.py
+++ data_generator.py
@@ -1,31 +1,41 @@
-from __future__ import absolute_import, division, print_function
-
 import numpy as np
+import torch
 
 target_to_text = {
-    '0': 'zero',
-    '1': 'one',
-    '2': 'two',
-    '3': 'three',
-    '4': 'four',
-    '5': 'five',
-    '6': 'six',
-    '7': 'seven',
-    '8': 'eight',
-    '9': 'nine',
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
 }
 
-EOS = '#'
+EOS = "#"
 
 input_characters = " ".join(target_to_text.values())
-valid_characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', EOS] + \
-                   list(set(input_characters))
+valid_characters = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    EOS,
+] + list(set(input_characters))
 
 
 def print_valid_characters():
-    l = ''
+    l = ""
     for i, c in enumerate(valid_characters):
-        l += "\'%s\'=%i,\t" % (c, i)
+        l += "'%s'=%i,\t" % (c, i)
     print("Number of valid characters:", len(valid_characters))
     print(l)
 
@@ -33,7 +43,9 @@ def print_valid_characters():
 ninput_chars = len(valid_characters)
 
 
-def generate(batch_size=100, min_len=3, max_len=3, invalid_set=set()):
+def generate(
+    num_batches=10, batch_size=100, min_len=3, max_len=3, invalid_set=set()
+):
     """
     Generates random sequences of integers and translates them to text i.e. 1->'one'.
 
@@ -50,108 +62,185 @@ def generate(batch_size=100, min_len=3, max_len=3, invalid_set=set()):
     text_targets_out = []
     int_targets_in = []
     int_targets_out = []
-    iterations = 0
+    inputs = []
+    targets_in = []
+    targets_out = []
+    batch_target_max_len = np.zeros((1, num_batches))
+    batch_input_max_len = np.zeros((1, num_batches))
     _printed_warning = False
-    while len(text_inputs) < batch_size:
-        iterations += 1
+    # loop through number of batches
+    for i in range(num_batches):
+        print("batch ", i, " starting")
+        temp_text_inputs = []
+        temp_int_inputs = []
+        temp_text_targets_in = []
+        temp_text_targets_out = []
+        temp_int_targets_in = []
+        temp_int_targets_out = []
+        iterations = 0
+        # while loop until number of rows per batch is reached
+        while len(temp_text_inputs) < batch_size:
+            iterations += 1
 
-        # choose random sequence length
-        tar_len = np.random.randint(min_len, max_len + 1)
+            # choose random sequence length
+            tar_len = np.random.randint(min_len, max_len + 1)
 
-        # list of text digits
-        text_target = inp_str = "".join(map(str, np.random.randint(0, 10, tar_len)))
-        text_target_in = EOS + text_target
-        text_target_out = text_target + EOS
+            # list of text digits
+            text_target = inp_str = "".join(
+                map(str, np.random.randint(0, 10, tar_len))
+            )
+            text_target_in = EOS + text_target
+            text_target_out = text_target + EOS
 
-        # generate the targets as a list of integers
-        int_target_in = map(lambda c: valid_characters.index(c), text_target_in)
-        int_target_in = list(int_target_in)
-        int_target_out = map(lambda c: valid_characters.index(c), text_target_out)
-        int_target_out = list(int_target_out)
+            # generate the targets as a list of integers
+            int_target_in = map(
+                lambda c: valid_characters.index(c), text_target_in
+            )
+            int_target_in = list(int_target_in)
 
-        # generate the text input
-        text_input = " ".join(map(lambda k: target_to_text[k], inp_str))
+            int_target_out = map(
+                lambda c: valid_characters.index(c), text_target_out
+            )
+            int_target_out = list(int_target_out)
 
-        # generate the inputs as a list of integers
-        int_input = map(lambda c: valid_characters.index(c), text_input)
-        int_input = list(int_input)
+            # generate the text input
+            text_input = " ".join(map(lambda k: target_to_text[k], inp_str))
 
-        if not _printed_warning and iterations > 5 * batch_size:
-            print("WARNING: doing a lot of iterations because I'm trying to generate a batch that does not"
-                  " contain samples from 'invalid_set'.")
-            _printed_warning = True
+            # generate the inputs as a list of integers
+            int_input = map(lambda c: valid_characters.index(c), text_input)
+            int_input = list(int_input)
 
-        if text_target_out in invalid_set:
-            continue
+            if not _printed_warning and iterations > 5 * batch_size:
+                print(
+                    "WARNING: doing a lot of iterations because I'm trying to generate a batch that does not"
+                    " contain samples from 'invalid_set'."
+                )
+                _printed_warning = True
 
-        text_inputs.append(text_input)
-        int_inputs.append(int_input)
-        text_targets_in.append(text_target_in)
-        text_targets_out.append(text_target_out)
-        int_targets_in.append(int_target_in)
-        int_targets_out.append(int_target_out)
-        # print('text_inputs', type(text_inputs), text_inputs)
-        # print('int_inputs', type(int_inputs), int_inputs)
-        # print('text_targets_in', type(text_targets_in), text_targets_in)
-        # print('text_targets_out', type(text_targets_out), text_targets_out)
-        # print('int_targets_in', type(int_targets_in), int_targets_in)
-        # print('int_targets_out', type(int_targets_out), int_targets_out)
+            if text_target_out in invalid_set:
+                continue
+            # append created row to temp arrays
+            temp_text_inputs.append(text_input)
+            temp_int_inputs.append(int_input)
+            temp_text_targets_in.append(text_target_in)
+            temp_text_targets_out.append(text_target_out)
+            temp_int_targets_in.append(int_target_in)
+            temp_int_targets_out.append(int_target_out)
+        # turn the temp arrays into tensors and pad them to max length
 
-    # create the input matrix, mask and seq_len - note that we zero pad the shorter sequences.
-    max_input_len = max([len(list(i)) for i in int_inputs])
-    inputs = np.zeros((batch_size, max_input_len))
-    # input_masks = np.zeros((batch_size,max_input_len))
-    for (i, inp) in enumerate(int_inputs):
-        cur_len = len(list(inp))
-        inputs[i, :cur_len] = inp
-        # input_masks[i,:cur_len] = 1
-    # inputs_seqlen = np.asarray(map(len, int_inputs))
-    inputs_seqlen = np.asarray([len(i) for i in int_inputs])
-    # print('max_input_len', type(max_input_len), max_input_len)
-    # print('inputs_seqlen', type(inputs_seqlen), inputs_seqlen)
+        # append completed temp batch array to full array of batches
+        text_inputs.append(temp_text_inputs)
+        int_inputs.append(temp_int_inputs)
+        text_targets_in.append(temp_text_targets_in)
+        text_targets_out.append(temp_text_targets_out)
+        int_targets_in.append(temp_int_targets_in)
+        int_targets_out.append(temp_int_targets_out)
 
-    # max_target_in_len = max(map(len, int_targets_in))
-    max_target_in_len = max([len(list(i)) for i in int_targets_in])
-    targets_in = np.zeros((batch_size, max_target_in_len))
-    targets_mask = np.zeros((batch_size, max_target_in_len))
-    for (i, tar) in enumerate(int_targets_in):
-        cur_len = len(tar)
-        targets_in[i, :cur_len] = tar
-    # targets_seqlen = np.asarray(map(len, int_targets_in))
-    targets_seqlen = np.asarray([len(i) for i in int_targets_in])
-    # print('int_targets_in', type(int_targets_in), int_targets_in)
-    # print('max_target_in_len', type(max_target_in_len), max_target_in_len)
-    # print()
-    # print('max_target_in_len', type(max_target_in_len), max_target_in_len)
-    # print('targets_seqlen', type(targets_seqlen), targets_seqlen)
+        max_target_out_len = max(map(len, int_targets_out[-1]))
+        max_input_len = max(map(len, int_inputs[-1]))
+        targets_mask = np.zeros((batch_size, max_target_out_len))
+        #         targets_out = np.zeros((batch_size, max_target_in_len))
+        for (j, tar) in enumerate(int_targets_out[-1]):
+            cur_len = len(tar)
+            targets_mask[j, :cur_len] = 1
 
-    max_target_out_len = max(map(len, int_targets_out))
-    targets_out = np.zeros((batch_size, max_target_in_len))
-    for (i, tar) in enumerate(int_targets_out):
-        cur_len = len(tar)
-        targets_out[i, :cur_len] = tar
-        targets_mask[i, :cur_len] = 1
+        len_arr = [-len(thing) for thing in temp_int_inputs]
+        sorted_arr = np.argsort(len_arr)
+        sorted_inputs = [temp_int_inputs[k] for k in sorted_arr]
+        add_inputs = np.zeros((batch_size, max_input_len))
+        for row in range(len(sorted_arr)):
+            cur_len = len(sorted_inputs[row])
+            add_inputs[row, :cur_len] = sorted_inputs[row]
+        inputs.append(add_inputs.astype("int32"))
 
-    print("Generated batch length {} from {} iterations".format(len(text_inputs), iterations))
+        len_arr = [-len(thing) for thing in temp_int_targets_in]
+        sorted_arr = np.argsort(len_arr)
+        sorted_targets_in = [temp_int_targets_in[k] for k in sorted_arr]
+        add_targets_in = np.zeros((batch_size, max_target_out_len))
+        for row in range(len(sorted_arr)):
+            cur_len = len(sorted_targets_in[row])
+            add_targets_in[row, :cur_len] = sorted_targets_in[row]
+        targets_in.append(add_targets_in.astype("int32"))
 
-    return inputs.astype('int32'), \
-        inputs_seqlen.astype('int32'), \
-        targets_in.astype('int32'), \
-        targets_out.astype('int32'), \
-        targets_seqlen.astype('int32'), \
-        targets_mask.astype('float32'), \
-        text_inputs, \
-        text_targets_in, \
-        text_targets_out
+        len_arr = [-len(thing) for thing in temp_int_targets_out]
+        sorted_arr = np.argsort(len_arr)
+        sorted_targets_out = [temp_int_targets_out[k] for k in sorted_arr]
+        add_targets_out = np.zeros((batch_size, max_target_out_len))
+        for row in range(len(sorted_arr)):
+            cur_len = len(sorted_targets_out[row])
+            add_targets_out[row, :cur_len] = sorted_targets_out[row]
+        targets_out.append(add_targets_out.astype("int32"))
+
+        """
+        almost_inputs=Variable(torch.zeros((len(temp_int_inputs), input_seq_lengths.max()))).long()
+        for idx, (seq, seqlen) in enumerate(zip(temp_int_inputs, input_seq_lengths)):
+            cur_len=len(seq)
+            almost_inputs[idx, :cur_len] = torch.LongTensor(seq)
+        inputs.append(almost_inputs)
+
+        almost_targets_in=Variable(torch.zeros((len(temp_int_targets_in), target_in_seq_lengths.max()))).long()
+        for idx, (seq, seqlen) in enumerate(zip(temp_int_targets_in, target_in_seq_lengths)):
+            cur_len=len(seq)
+            almost_targets_in[idx, :cur_len] = torch.LongTensor(seq)
+        targets_in.append(almost_targets_in)
+
+        target_out_seq_lengths= torch.LongTensor(list(map(len, temp_int_targets_out)))
+        almost_targets_out=Variable(torch.zeros((len(temp_int_targets_out), target_out_seq_lengths.max()))).long()
+        for idx, (seq, seqlen) in enumerate(zip(temp_int_targets_out, target_out_seq_lengths)):
+            cur_len=len(seq)
+            almost_targets_out[idx, :cur_len] = torch.LongTensor(seq)
+        targets_out.append(almost_targets_out)
+        """
+        target_in_seq_lengths = torch.LongTensor(
+            list(map(len, temp_int_targets_in))
+        )
+        input_seq_lengths = torch.LongTensor(list(map(len, temp_int_inputs)))
+
+        batch_target_max_len[0, i] = target_in_seq_lengths.max()
+        batch_input_max_len[0, i] = input_seq_lengths.max()
+        print("batch ", i, " ended with ", len(text_targets_in[-1]))
+
+    print(
+        "Generated batch length {} from {} iterations".format(
+            len(text_inputs), iterations
+        )
+    )
+
+    return (
+        inputs,
+        batch_input_max_len.astype("int32"),
+        targets_in,
+        targets_out,
+        batch_target_max_len.astype("int32"),
+        targets_mask.astype("float32"),
+        text_inputs,
+        text_targets_in,
+        text_targets_out,
+    )
 
 
 def main():
     batch_size = 3
-    inputs, inputs_seqlen, targets_in, targets_out, targets_seqlen, targets_mask, \
-        text_inputs, text_targets_in, text_targets_out = \
-        generate(batch_size=batch_size, max_len=4, min_len=2)
+    (
+        inputs,
+        inputs_seqlen,
+        targets_in,
+        targets_out,
+        targets_seqlen,
+        targets_mask,
+        text_inputs,
+        text_targets_in,
+        text_targets_out,
+    ) = generate(8, 1000, min_len=2, max_len=4)
 
-    print("input types:", inputs.dtype, inputs_seqlen.dtype, targets_in.dtype, targets_out.dtype, targets_seqlen.dtype)
+    # print(
+    #     "input types:",
+    #     inputs.dtype,
+    #     inputs_seqlen.dtype,
+    #     targets_in.dtype,
+    #     targets_out.dtype,
+    #     targets_seqlen.dtype,
+    # )
     print_valid_characters()
     print("Stop/start character = #")
 
@@ -159,13 +248,14 @@ def main():
         print("\nSAMPLE", i)
         print("TEXT INPUTS:\t\t\t", text_inputs[i])
         print("ENCODED INPUTS:\t\t\t", inputs[i])
-        print("INPUTS SEQUENCE LENGTH:\t", inputs_seqlen[i])
+        print("INPUTS SEQUENCE LENGTH:\t", inputs_seqlen)
         print("TEXT TARGETS INPUT:\t\t", text_targets_in[i])
         print("TEXT TARGETS OUTPUT:\t", text_targets_out[i])
         print("ENCODED TARGETS INPUT:\t", targets_in[i])
         print("ENCODED TARGETS OUTPUT:\t", targets_out[i])
-        print("TARGETS SEQUENCE LENGTH:", targets_seqlen[i])
+        print("TARGETS SEQUENCE LENGTH:", targets_seqlen)
         print("TARGETS MASK:\t\t\t", targets_mask[i])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
