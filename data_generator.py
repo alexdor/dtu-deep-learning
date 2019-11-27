4788551017ab84ef0aaa600a635c0024fc6cf056
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-27 16:56:33 +0100

    feat(data_gen): Generate masks per batch

--- data_generator.py
+++ data_generator.py
@@ -66,6 +66,7 @@ def generate(
     targets_in = []
     targets_out = []
     batch_target_max_len = np.zeros((1, num_batches))
+    targets_mask = []
     batch_input_max_len = np.zeros((1, num_batches))
     _printed_warning = False
     # loop through number of batches
@@ -138,59 +139,27 @@ def generate(
 
         max_target_out_len = max(map(len, int_targets_out[-1]))
         max_input_len = max(map(len, int_inputs[-1]))
-        targets_mask = np.zeros((batch_size, max_target_out_len))
-        #         targets_out = np.zeros((batch_size, max_target_in_len))
-        for (j, tar) in enumerate(int_targets_out[-1]):
-            cur_len = len(tar)
-            targets_mask[j, :cur_len] = 1
-
+        targets_mask_tmp = np.zeros((batch_size, max_target_out_len))
+        add_targets_out = np.zeros((batch_size, max_target_out_len))
         len_arr = [-len(thing) for thing in temp_int_inputs]
         sorted_arr = np.argsort(len_arr)
-        sorted_inputs = [temp_int_inputs[k] for k in sorted_arr]
-        add_inputs = np.zeros((batch_size, max_input_len))
-        for row in range(len(sorted_arr)):
-            cur_len = len(sorted_inputs[row])
-            add_inputs[row, :cur_len] = sorted_inputs[row]
-        inputs.append(add_inputs.astype("int32"))
-
-        len_arr = [-len(thing) for thing in temp_int_targets_in]
-        sorted_arr = np.argsort(len_arr)
-        sorted_targets_in = [temp_int_targets_in[k] for k in sorted_arr]
         add_targets_in = np.zeros((batch_size, max_target_out_len))
-        for row in range(len(sorted_arr)):
-            cur_len = len(sorted_targets_in[row])
-            add_targets_in[row, :cur_len] = sorted_targets_in[row]
+        add_inputs = np.zeros((batch_size, max_input_len))
+        for short_index, row in enumerate(sorted_arr):
+            tmp_element = temp_int_inputs[row]
+            add_inputs[short_index, : len(tmp_element)] = tmp_element
+
+            tmp_element = temp_int_targets_in[row]
+            add_targets_in[short_index, : len(tmp_element)] = tmp_element
+
+            tmp_element = temp_int_targets_out[row]
+            add_targets_out[short_index, : len(tmp_element)] = tmp_element
+            targets_mask_tmp[short_index, : len(tmp_element)] = 1
+
+        targets_mask.append(targets_mask_tmp)
+        inputs.append(add_inputs.astype("int32"))
         targets_in.append(add_targets_in.astype("int32"))
-
-        len_arr = [-len(thing) for thing in temp_int_targets_out]
-        sorted_arr = np.argsort(len_arr)
-        sorted_targets_out = [temp_int_targets_out[k] for k in sorted_arr]
-        add_targets_out = np.zeros((batch_size, max_target_out_len))
-        for row in range(len(sorted_arr)):
-            cur_len = len(sorted_targets_out[row])
-            add_targets_out[row, :cur_len] = sorted_targets_out[row]
         targets_out.append(add_targets_out.astype("int32"))
-
-        """
-        almost_inputs=Variable(torch.zeros((len(temp_int_inputs), input_seq_lengths.max()))).long()
-        for idx, (seq, seqlen) in enumerate(zip(temp_int_inputs, input_seq_lengths)):
-            cur_len=len(seq)
-            almost_inputs[idx, :cur_len] = torch.LongTensor(seq)
-        inputs.append(almost_inputs)
-
-        almost_targets_in=Variable(torch.zeros((len(temp_int_targets_in), target_in_seq_lengths.max()))).long()
-        for idx, (seq, seqlen) in enumerate(zip(temp_int_targets_in, target_in_seq_lengths)):
-            cur_len=len(seq)
-            almost_targets_in[idx, :cur_len] = torch.LongTensor(seq)
-        targets_in.append(almost_targets_in)
-
-        target_out_seq_lengths= torch.LongTensor(list(map(len, temp_int_targets_out)))
-        almost_targets_out=Variable(torch.zeros((len(temp_int_targets_out), target_out_seq_lengths.max()))).long()
-        for idx, (seq, seqlen) in enumerate(zip(temp_int_targets_out, target_out_seq_lengths)):
-            cur_len=len(seq)
-            almost_targets_out[idx, :cur_len] = torch.LongTensor(seq)
-        targets_out.append(almost_targets_out)
-        """
         target_in_seq_lengths = torch.LongTensor(
             list(map(len, temp_int_targets_in))
         )
@@ -212,7 +181,7 @@ def generate(
         targets_in,
         targets_out,
         batch_target_max_len.astype("int32"),
-        targets_mask.astype("float32"),
+        targets_mask,
         text_inputs,
         text_targets_in,
         text_targets_out,
@@ -231,16 +200,8 @@ def main():
         text_inputs,
         text_targets_in,
         text_targets_out,
-    ) = generate(8, 1000, min_len=2, max_len=4)
+    ) = generate(8, 10, min_len=2, max_len=4)
 
-    # print(
-    #     "input types:",
-    #     inputs.dtype,
-    #     inputs_seqlen.dtype,
-    #     targets_in.dtype,
-    #     targets_out.dtype,
-    #     targets_seqlen.dtype,
-    # )
     print_valid_characters()
     print("Stop/start character = #")
 
